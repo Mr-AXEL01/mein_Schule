dfd96956f13532a902a4845225b5c6923ad5b5a1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-01 10:30:00 +0100

    check if course's name is unique

--- js/student.js
+++ js/student.js
@@ -98,10 +98,10 @@ class StudentManager {
                 const row = document.createElement('tr');
                 row.style.cursor = 'pointer';
                 row.innerHTML = `
-                    <td class="clickable border px-4 py-2">${student.id}</td>
-                    <td class="clickable border px-4 py-2">${student.name}</td>
-                    <td class="clickable border px-4 py-2">${student.age}</td>
-                    <td class="flex justify-center gap-4 px-6 py-2">
+                    <td class="clickable px-4 py-2">${student.id}</td>
+                    <td class="clickable px-4 py-2">${student.name}</td>
+                    <td class="clickable px-4 py-2">${student.age}</td>
+                    <td class="flex justify-center border gap-4 px-6 py-2">
                         <span class="icon-button icon-update" onclick="studentManager.editStudent(${student.id})">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="orange" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
